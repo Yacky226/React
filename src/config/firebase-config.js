2c82b1d6ecfb699aa9e2127971a2dd9963269961
Yacouba <sawadogoyacouba52@gmail.com>
2024-07-23 11:21:24 +0100

    Bouton

---
+++ src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqz214H08zBiRVhAs1xiulUHcpcYbUypM",
+  authDomain: "coders-16324.firebaseapp.com",
+  projectId: "coders-16324",
+  storageBucket: "coders-16324.appspot.com",
+  messagingSenderId: "586479146681",
+  appId: "1:586479146681:web:df012245217f85e5bced68"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,52 @@
 import { Seo } from "@/ui/components/seo";
+import { Button } from "@/ui/design-system/typography/button/button";
+import { Typography } from "@/ui/design-system/typography/typography";
 import Head from "next/head";
 export default function Home() {
   return (
     <>
     <Seo title="Coders" description="Description"/>
-    <h1>Hello</h1>
-    </>
+    <div className="flex items-center gap-4 p-10 ">
+    <Button size="small"> Accent</Button>
+    <Button size="small" variant="secondary"> Secondary</Button>
+    <Button size="small" variant="outline"> Outline</Button>
+    <Button size="small" variant="disabled" disabled> disabled</Button>
+    </div>
+    
+    <div className="flex items-center gap-4 p-10 ">
+    <Button> Accent</Button>
+    <Button variant="secondary"> Secondary</Button>
+    <Button variant="outline"> Outline</Button>
+    <Button variant="disabled" disabled> disabled</Button>
+    </div>
+    
+    <div className="flex items-center gap-4 p-10 ">
+    <Button size="large"> Accent</Button>
+    <Button size="large" variant="secondary"> Secondary</Button>
+    <Button size="large" variant="outline"> Outline</Button>
+    <Button size="large" variant="disabled" disabled> disabled</Button>
+    </div>
+     {/* <div className="space-y-5">
+    <Typography variant="display" component="div" themes="primary">
+      Coders
+    </Typography>
+    <Typography variant="h1" component="div" themes="secondary">
+      Coders Monkey
+    </Typography>
+    <Typography variant="Lead" component="div">
+      Coders
+    </Typography>
+    <Typography variant="body-sm" component="div">
+      Coders
+    </Typography>
+    <Typography variant="caption4" component="div">
+      Coders
+    </Typography>
+    <Typography variant="caption4" weight="medium" component="div">
+      Coders
+    </Typography>
+    </div> */}
+  </>
+   
   );
 }
